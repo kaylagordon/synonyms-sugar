47b4cca05c65d6d501a8ab9f412b56cdf53296e6
Kayla Wood <kaylaewood@gmail.com>
2020-03-02 12:27:19 -0700

    Break Form and Title into separate components

---
+++ src/Form.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<form>
+	<input type='text' placeholder='Enter a word here...'/>
+	<button className='submit-word-button'>Go!</button>
+</form>
+
+<style>
+
+</style>
---
+++ src/Title.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<section className='title'>
+	<h1>SYNONYMS</h1>
+	<h2>& sugar</h2>
+</section>
+
+<style>
+
+</style>
