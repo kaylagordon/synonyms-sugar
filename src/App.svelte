16f6b81368784507d09b58530ff6c01a233473cd
Kayla Wood <kaylaewood@gmail.com>
2020-03-02 12:06:48 -0700

    Add heading and image

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,25 @@
+<script>
 
+</script>
+
+<main>
+	<h1>SYNONYMS</h1>
+	<h2>& sugar</h2>
+	<img src='./images/background-image.png' />
+	<p></p>
+</main>
+
+<style>
+	main {
+		background-color: #545454;
+	}
+
+	h1 {
+
+	}
+
+	h2 {
+
+	}
+
+</style>
