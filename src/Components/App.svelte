5d742915f085f803543b9c6458f4d62a488d5147
Kayla Wood <kaylaewood@gmail.com>
2020-03-03 15:57:15 -0700

    Adjust background image styling

--- src/Components/App.svelte
+++ src/Components/App.svelte
@@ -10,15 +10,20 @@
 		<Form />
 	</div>
 	<WordBox />
-	<img alt='donuts' src='./images/background-image.png' />
 </main>
 
 <style>
 
 	main {
 		background-color: #545454;
+		background-image: url('../images/background-image.png');
+		background-size: 40vw;
+		background-repeat: no-repeat;
+		background-position: left bottom;
+		background-attachment: fixed;
 		margin: -2vw;
 		padding: 4vw;
+		height: 100%;
 	}
 
 	div {
